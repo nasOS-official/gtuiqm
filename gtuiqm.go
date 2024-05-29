47c1540da9e972bf29289f861069a29114f8179a
nasOS-official <petruhin-enot@yandex.ru>
2024-05-29 12:53:10 +0400

    First Commit

---
+++ gtuiqm.go
@@ -0,0 +1,70 @@
+// Copyright (C) Egor
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// GNU Lesser General Public Licence is available at
+// http://www.gnu.org/copyleft/lesser.html
+package gtuiqm
+
+import (
+	"fmt"
+
+	"atomicgo.dev/keyboard"
+	"atomicgo.dev/keyboard/keys"
+)
+
+func ShowMenu(menu []string, title string, fullline ...bool) {
+	if fullline == nil {
+		fullline = []bool{false}
+	}
+	elem := 0
+	CreateMenu(elem, menu, title, fullline[0])
+	keyboard.Listen(func(key keys.Key) (stop bool, err error) {
+
+		switch key.Code {
+		case keys.Up:
+			if elem != 0 {
+				elem--
+			} else {
+				elem = len(menu) - 1
+			}
+			CreateMenu(elem, menu, title, fullline[0])
+
+		case keys.Down:
+
+			if elem != len(menu)-1 {
+				elem++
+			} else {
+				elem = 0
+			}
+
+			CreateMenu(elem, menu, title, fullline[0])
+
+		case keys.Enter:
+			return true, nil
+		}
+
+		return false, nil // Return false to continue listening
+	})
+
+}
+func CreateMenu(elem int, menu []string, title string, fullline ...bool) {
+	if fullline == nil {
+		fullline = []bool{false}
+	}
+
+	fmt.Printf("\033c")
+	fmt.Printf("\x1b[35m" + title + "\x1b[0m\n")
+	for i := 0; i < len(menu); i++ {
+		if elem == i {
+			fmt.Printf("\x1b[47;30m" + menu[i] + "\x1b[0m\n")
+		} else {
+			fmt.Println(menu[i])
+		}
+	}
+}
